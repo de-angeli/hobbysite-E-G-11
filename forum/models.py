df199fbb8fbb5d5f438c5bbfb8f148c5f3372fce
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-03-01 22:50:56 +0800

    created models for forum

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        PostCategory,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='posts'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]
