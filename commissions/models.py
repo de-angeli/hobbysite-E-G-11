e69a53ad2725ecee6c0b02d85886fdba315608db
Anne Francine Benito <annefrancine.benito@gmail.com>
2025-03-05 20:04:26 +0800

    Modified views.py and detail template, Created admin panel, Added object instances

--- commissions/models.py
+++ commissions/models.py
@@ -5,14 +5,17 @@ class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     people_required = models.IntegerField()
-    created_on = models.DateTimeField(null=False)
-    updated_on = models.DateTimeField(null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['created_on']
 
     def get_absolute_url(self):
         return reverse('commissions:commission-detail', args=[self.pk])
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     commission = models.ForeignKey(
@@ -22,8 +25,8 @@ class Comment(models.Model):
         related_name="comment"
     )
     entry = models.TextField()
-    created_on = models.DateTimeField(null=False)
-    updated_on = models.DateTimeField(null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_on']
