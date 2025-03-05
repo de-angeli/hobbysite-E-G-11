219a36401c3f972d6d1180edd518a6bcb052d043
Anne Francine Benito <annefrancine.benito@gmail.com>
2025-03-05 19:34:37 +0800

    Modified urls.py and created list and detail view of Commission

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import CommissionListView, CommissionDetailView
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('commission/list', CommissionListView.as_view(), name='commission-list'),
+    path('commissions/detail/<int:pk>', CommissionDetailView.as_view(), name='commission-detail')
     ]
 
 app_name = "commissions"
