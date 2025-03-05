786791a8fb403c8c8b8f9ce8bbfc2891ecbe7889
Anne Francine Benito <annefrancine.benito@gmail.com>
2025-03-05 19:25:01 +0800

    Created commission and comment models; Made migrations

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(null=False)
+    updated_on = models.DateTimeField(null=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def get_absolute_url(self):
+        return reverse('commissions:commission-detail', args=[self.pk])
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="comment"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(null=False)
+    updated_on = models.DateTimeField(null=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
