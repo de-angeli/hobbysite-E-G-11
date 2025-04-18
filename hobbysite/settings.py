ccc0fcadae3e79d7fc2ae4f08216ffd8bcb05178
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-04-18 18:12:52 +0800

    modified project settings

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'merchstore',
     'blog',
     'wiki',
+    'user_management'
 ]
 
 MIDDLEWARE = [
@@ -124,8 +125,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+STATIC_URL = '/static/'
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
