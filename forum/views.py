fb4d115ce32ff599e5e3546238c57b949af8e799
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-04-23 13:31:57 +0800

    changed Post to Thread

--- forum/views.py
+++ forum/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Post, PostCategory
+from .models import Thread, ThreadCategory
 
 
 class CategoryListView(ListView):
-    model = PostCategory
+    model = ThreadCategory
     template_name = 'forum/thread_list.html'
 
 
 class PostDetailView(DetailView):
-    model = Post
+    model = Thread
     template_name = 'forum/thread_detail.html'
 
 def post_list(request):
-    categories = PostCategory.objects.prefetch_related('posts')
-    posts = Post.objects.all()
+    categories = ThreadCategory.objects.prefetch_related('posts')
+    posts = Thread.objects.all()
     ctx = {
         "categories": categories,
         "posts" : posts
@@ -24,5 +24,5 @@ def post_list(request):
     return render(request, 'forum/thread_list.html', ctx)
 
 def post_detail(request, pk):
-    ctx = { "post" : Post.objects.get(pk=pk) }
+    ctx = { "post" : Thread.objects.get(pk=pk) }
     return render(request, 'forum/thread_detail.html', ctx)
