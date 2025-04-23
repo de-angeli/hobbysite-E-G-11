60acc0283d0400892a45f6b9064743df5d5d24d3
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-04-23 12:54:19 +0800

    removed unnecessary paths

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -31,6 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('register/', user_views.register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
