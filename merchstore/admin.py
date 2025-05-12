3c3bb0235fdbb333a05e4fb0167f33005609785b
Enrico Cruel <pio.cruel@student.ateneo.edu>
2025-05-12 20:50:35 +0800

    Initial commit of updated merchstore app, including product editing and adding and cart list and transaction list.

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductType
+from .models import Product, ProductType, Transaction
 
 
 class ProductTypeAdmin(admin.ModelAdmin):
@@ -10,10 +10,17 @@ class ProductTypeAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
-    list_display = ('name', 'product_type', 'price')
-    list_filter = ('product_type',)
+    list_display = ('name', 'product_type', 'owner', 'price', 'stock', 'status')
+    list_filter = ('product_type','status','owner')
     search_fields = ('name',)
-    fields = ('name', 'product_type', 'description', 'price')
+    fields = ('name', 'product_type', 'owner', 'description', 'price', 'stock', 'status')
+
+class TransactionAdmin(admin.ModelAdmin):
+    model = Transaction
+    list_display = ('buyer', 'product', 'status', 'amount', )
+    fields = ('buyer', 'product', 'status', 'amount')
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- merchstore/models.py
+++ merchstore/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -16,6 +17,12 @@ class ProductType(models.Model):
 
 
 class Product(models.Model):
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('On Sale', 'On Sale'),
+        ('Out of Stock', 'Out of Stock'),
+    ]
+
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
         ProductType,
@@ -23,14 +30,70 @@ class Product(models.Model):
         null=True,
         related_name='item_list'
     ) 
+    owner = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+    )
     description = models.TextField()
-    price = models.DecimalField(max_digits=100,decimal_places=2)
+    price = models.DecimalField(
+        max_digits=100,
+        decimal_places=2
+        )
+    stock = models.IntegerField(default=100)
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='Available'
+    )
 
     class Meta:
         ordering = ['name']
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.status = 'Out of Stock'
+        super().save(*args, **kwargs)
+        
+    def get_absolute_url(self):
+        return reverse('merchstore:item-detail', args=[self.pk])
+    
+class Transaction(models.Model):
+    STATUS_CHOICES = [
+        ('On Cart', 'On Cart'),
+        ('To Pay', 'To Pay'),
+        ('To Ship', 'To Ship'),
+        ('To Receive', 'To Receive'),
+        ('Delivered', 'Delivered'),
+    ]
+
+    buyer = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+    ) 
+    amount = models.IntegerField(default=0)
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='On Cart'
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)  # Sets date and time on creation
+    
+    def __str__(self):
+        if self.product:
+            return self.product.name
+        return self.id
         
     def get_absolute_url(self):
         return reverse('merchstore:item-detail', args=[self.pk])
