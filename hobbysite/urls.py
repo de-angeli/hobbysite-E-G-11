c4b2ff79acd61588a50918c4159b5a4b300ce068
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-12 16:34:07 +0800

    fixed urls to accommodate static images

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from user_management import views as user_views
@@ -32,3 +34,5 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', user_views.register_view, name='register'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
