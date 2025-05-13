89718bb00818016e00e5cba7cc02e403cf0e0e25
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-13 21:53:18 +0800

    updated models

--- merchstore/models.py
+++ merchstore/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.urls import reverse
 from user_management.models import Profile
 
-from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
