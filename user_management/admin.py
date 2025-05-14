73bd54ce46fdda91ea29bea6f08e91dfdfd5081f
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-14 20:48:47 +0800

    updated admin.py of user_management

--- user_management/admin.py
+++ user_management/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'display_name', 'email')  # You can add more fields here
-    search_fields = ('user__username', 'display_name', 'email')  # Allows searching by these fields
+    list_display = ('user', 'display_name', 'email')
+    search_fields = ('user__username', 'display_name', 'email')
 
 admin.site.register(Profile, ProfileAdmin)
