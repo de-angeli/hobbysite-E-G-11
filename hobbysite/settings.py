1a11387101b4f3b497c60bfac1fa24e074da6b63
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-15 21:06:08 +0800

    debug project-level settings

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'hobbysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(str(BASE_DIR, 'templates'))],
+        'DIRS': [str(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
