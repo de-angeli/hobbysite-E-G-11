64b7000265e0ab48ccdf0e2d32d5325e1e4a924c
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-15 21:53:52 +0800

    debug project-level settings

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -101,7 +101,7 @@ db_info = urlparse(DATABASE_URL)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': db_info.path[1:],
+        'NAME': 'db',
         'USER': db_info.username,
         'PASSWORD': db_info.password,
         'HOST': db_info.hostname,
