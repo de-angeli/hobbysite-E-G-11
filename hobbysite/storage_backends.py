b5c29c5d70a1bbf3da5f5888edf09bf9e9cce0c4
Jameel Angeli Abarico <abarico.angeli.jameel@gmail.com>
2025-05-19 19:24:16 +0800

    modified setting to fix image upload

--- hobbysite/storage_backends.py
+++ hobbysite/storage_backends.py
@@ -2,11 +2,6 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticFilesStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public-read'
-
-
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
